723e59c1a6df4ad7318790758433966bd99136d3
nickd <nickd@192.168.1.113>
2021-12-28 22:30:06 -0500

    Day8 homework Question 3

---
+++ src/test/java/Question3.java
@@ -0,0 +1,37 @@
+
+
+public class Question3 {
+	
+	public static void main(String [] args) {
+		
+		//for loop
+		
+		int sum=0;
+		
+		for (int number = 20; number <= 30; number++) {
+			
+			sum=sum+number;
+			
+		
+			System.out.println(sum);
+			System.out.println("Average:" + sum/11);
+			
+			
+			
+			
+			
+			
+			
+			
+		
+		
+		
+		
+			
+		}
+			
+	}
+	
+	
+
+}
