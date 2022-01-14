096b5160b6d4cb965b7ac2ea9ab893ee4678187c
nickd <nickd@LAPTOP-11BB3S0F.lan1>
2022-01-14 17:54:59 -0500

    Broken code

---
+++ src/test/java/BrokenCode.java
@@ -0,0 +1,64 @@
+
+
+public class BrokenCode {
+	
+public static void main(String[] args) {
+
+		double numOne =2.0; 
+		double numTwo= 4.0;
+		double numThree=3.0;
+		
+		System.out.println(numOne+numTwo+numThree);
+		
+				
+		for(int i= 0; i <= 10; i++){
+			System.out.println("The value of i id : +1");
+		}
+		System.out.println("Outside the loop");
+
+		int whoseFavNumber=23;
+		int whoseFavNumber=3;		
+		int whoseFavNumber=1;
+				
+		String myName="John Cena";
+		
+		
+		
+	public static void myName (String Name) {
+			System.out.println("My name is " + Name);
+		}
+	
+	
+	public static void addTwoNumbers(int numOne, int numTwo) {
+
+		int result = numOne + numTwo;
+
+		System.out.println(numOne + " + " + numTwo + " = " + result);
+		
+		
+	}
+	
+	public static void whosefavNumber(int i){
+		boolean result =false;
+		
+		
+		if ( i == 1) {
+			result = false;
+		}else if (i == 2 || i == 4 && i == 4) {
+			result = true;
+		} else if (i == 3) {
+			result = false;
+		}  else {
+			boolean result1 = false;
+		}
+		
+	
+	
+
+
+	
+}
+
+}
+
+
